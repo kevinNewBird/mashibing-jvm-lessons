07296369adf75c75990f3125b4bf9eb799027776
zhao.song <song0586@126.com>
2020-11-10 00:22:51 +0800

    feat(初始化):class文件格式

---
+++ JVM/src/main/java/com/mashibing/jvm/T01_TestClassComplie.java
@@ -0,0 +1,11 @@
+package com.mashibing.jvm;
+
+/***********************
+ * Description: TODO <BR>
+ * @author: zhao.song
+ * @date: 2020/11/10 0:17
+ * @version: 1.0
+ ***********************/
+public class T01_TestClassComplie {
+
+}
